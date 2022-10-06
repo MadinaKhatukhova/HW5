1192611e967165ed2fe8efc9e2e9767b01db23a9
Madina <madina-khat@yandex.ru>
2022-10-06 22:49:25 +0300

    Д/з 2 Задача 1,2,3

--- src/Main.java
+++ src/Main.java
@@ -35,10 +35,50 @@ public class Main {
         }
         if (place < 102)  {
             System.out.println("Вагон уже полностью забит!");
-        }}}}
-
-
-
-
-
         }
+
+
+
+
+        int age4 = 17;
+        if (age4 >= 18)  {
+                System.out.println("С совершеннолетием!");
+        } else {
+                System.out.println("До совершеннолетия осталось ... лет!");
+            }
+        int age5 = 8;
+        if (age5 >= 7)  {
+                System.out.println("Можешь ходить в школу!");
+        } else {
+                System.out.println("Не можешь ходить в школу!");
+        }
+        int age6 = 18;
+        if (age6 >= 18)  {
+            System.out.println("Можешь ходить в университет!");
+        } else  {
+            System.out.println("Не можешь ходить в университет!");
+                }
+        int age7 = 25;
+        if (age7 >= 24)  {
+            System.out.println("Можешь искать работу!");
+        } else  {
+            System.out.println("Не можешь искать работу!");
+        }
+        int place1 = 103;
+        if (place1 <= 102)  {
+            System.out.println("Вместимость 102");
+        } else  {
+            System.out.println("Вагон уже полностью забит!");
+        }}}}}
+
+
+
+
+
+
+
+
+
+
+
+
