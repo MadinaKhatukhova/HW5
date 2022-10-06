a8108ebc547006fcb340237a4e541e93e674328c
Madina <madina-khat@yandex.ru>
2022-10-06 21:08:19 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18)  {
+            System.out.println("С совершеннолетием!");
+        }
+
+        if (age < 18) {
+            System.out.println("До совершеннолетия осталось ... лет!");
+        }
+    }
+}
