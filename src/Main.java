0138a411598056e4b5b665a17c4b78589d014d27
Madina <madina-khat@yandex.ru>
2022-10-06 21:55:37 +0300

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,18 @@ public class Main {
         if (age >= 18)  {
             System.out.println("С совершеннолетием!");
         }
-
         if (age < 18) {
             System.out.println("До совершеннолетия осталось ... лет!");
         }
-    }
-}
+        int age1 = 8;
+        if (age1 >= 7)  {
+            System.out.println("Можешь ходить в школу!");
+        }
+        int age2 = 18;
+        if (age2 >= 18)  {
+            System.out.println("Можешь ходить в университет!");
+        }
+        int age3 = 25;
+        if (age3 >= 24)  {
+            System.out.println("Можешь искать работу!");
+    }}}
